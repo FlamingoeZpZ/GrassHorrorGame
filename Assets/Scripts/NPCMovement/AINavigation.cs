c0db98b1ee8d11a05ca6640a12f512b1af06585b
FreeAbrams2048 <123333060+FreeAbrams2048@users.noreply.github.com>
2024-04-07 20:13:43 -0400

    added navmesh and updated neighbour to use it

--- Assets/Scripts/NPCMovement/AINavigation.cs
+++ Assets/Scripts/NPCMovement/AINavigation.cs
@@ -1,40 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using UnityEngine.Polybrush;
-using Unity.Mathematics;
-using Unity.VisualScripting;
+using UnityEngine.AI;
 
 public class AINavigation : MonoBehaviour
 {
-	private Coroutine moving;
-	private Transform travelTarget;
-
+	public Transform travelTarget;
+	private NavMeshAgent agent;
+	[SerializeField] private string MainTarget;
 
 	[SerializeField] private float speed = 5;
-	[SerializeField] private float lookSpeed = 5;
-
 	void Start()
 	{
-		travelTarget = GameObject.Find("NavPoint").transform;
+		agent=GetComponent<NavMeshAgent>();
+		travelTarget = GameObject.Find(MainTarget).transform;
+		print(travelTarget);
+		agent.SetDestination(travelTarget.position);
+		agent.speed=speed;
 	}
 
 	void Update()
 	{
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(travelTarget.position - transform.position), lookSpeed * Time.deltaTime);
-
-		transform.position += transform.forward * speed * Time.deltaTime;
+		agent.SetDestination(travelTarget.position);
 	}
 
 	public IEnumerator DelayCoroutine(float delay)
 	{
 		float restoreSpeed = speed;
-		float restoreLookSpeed = lookSpeed;
 		speed = 0;
-		lookSpeed = 0;
 		yield return new WaitForSeconds(delay);
 		speed = restoreSpeed;
-		lookSpeed = restoreLookSpeed;
 	}
 }
